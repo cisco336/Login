e80175e6a6ede37da867a16ccf1a7ac2ba924fea
SEIJA002 <cisco336@gmail.com>
2018-12-20 15:20:21 -0500

    Eviroment created

---
+++ TheFront/src/app/environment/environment.ts
@@ -0,0 +1,14 @@
+export const Env = {
+    server: 'http://localhost',
+    // server = 'http://192.168.0.24',
+    // server = 'http://10.131.10.80',
+
+    port: '5000',
+    loggin: '/api/login',
+    register: '/api/register',
+    special: '/api/special',
+    // products: '/api/products',
+    // product: '/api/product',
+    // addProduct: '/add',
+    // contactMsg: '/api/msg/add'
+};
